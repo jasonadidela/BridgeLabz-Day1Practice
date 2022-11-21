0285faf5b28f888654080d666979b173f1f2d1c6
jasonadidela <jasonadidela@gmail.com>
2022-11-21 23:03:24 +0530

    Rename branch 1.4_nameCommandLine Branch to 1.4_sumCommandLine is created.

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Main class");
-        System.out.println("Welcome to 1.4_nameCommandLine Branch");
+        System.out.println("Welcome to 1.4_sumCommandLine Branch");
     }
 }
