67058144d09befd320c76d0f574bd0a07c7684ff
jasonadidela <jasonadidela@gmail.com>
2022-11-21 23:22:08 +0530

    1.4_sumCommandLine code is created.

--- Main.java
+++ Main.java
@@ -3,5 +3,16 @@ public class Main {
 
         System.out.println("Welcome to Main class");
         System.out.println("Welcome to 1.4_sumCommandLine Branch");
+        int sum = 0;
+        System.out.println("Calculates Sum for below Integers");
+        for(int i=0; i<args.length; i++){
+            System.out.println(args[i]);
+            try {
+                sum = sum + Integer.parseInt(args[i]);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid arguments: " + e.toString());
+            }
+        }
+        System.out.println("Sum :" + sum);
     }
 }
