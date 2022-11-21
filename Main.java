c8230797147a8c8506f2eeefd08c90b3f5040db0
jasonadidela <jasonadidela@gmail.com>
2022-11-21 23:18:30 +0530

    1.5_nameCommandLine code is created.

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Main class");
         System.out.println("Welcome to 1.5_nameCommandLine Branch Branch");
+        System.out.println("Your first argument is: "+args[0]);
     }
 }
