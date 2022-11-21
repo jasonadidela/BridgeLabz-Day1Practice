f99a4230b7416c8036d6e83d225498bce53aa9b9
jasonadidela <jasonadidela@gmail.com>
2022-11-21 23:26:14 +0530

    1.3_StringEqual code is created.

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
+//1.3 Write a program to check two strings are equal or not.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Main class");
         System.out.println("Welcome to New Branch 1.3_StringEqual");
+        Scanner var = new Scanner(System.in);
+        System.out.println("Enter username");
+        String userName = var.nextLine();
+        Scanner v = new Scanner(System.in);
+        System.out.println("Enter other username");
+        String otherUserName = v.nextLine();
+        if (userName.equals(otherUserName)) {
+        System.out.println("userName is matching");
+        } else {
+           System.out.println("userName is not matching");
+          }
     }
 }
