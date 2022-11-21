21c33b1ad76e66efd69d32873627c502e05d6e1f
jasonadidela <jasonadidela@gmail.com>
2022-11-21 20:25:49 +0530

    Created New Branch 1.1_DisplayAMessage and committing first commit.

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Main class");
+        System.out.println("Welcome to New Branch 1.1_DisplayAMessage");
     }
 }
